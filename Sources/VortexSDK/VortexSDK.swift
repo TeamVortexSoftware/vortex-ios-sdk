b85eb2737a7db1900bb55a5ef4ab1b5fc499d276
Marcelo Bukowski <marcelo.farias@gmail.com>
2026-02-11 15:35:03 -0300

    Bump version to 1.0.7-dev for development

--- Sources/VortexSDK/VortexSDK.swift
+++ Sources/VortexSDK/VortexSDK.swift
@@ -5,7 +5,7 @@ import CoreTelephony
 /// VortexSDK version information and namespace
 public enum VortexSDKInfo {
     /// Current version of the SDK
-    public static let version = "1.0.6"
+    public static let version = "1.0.7-dev"
     
     /// SDK name identifier
     public static let name = "VortexSDK-iOS"
